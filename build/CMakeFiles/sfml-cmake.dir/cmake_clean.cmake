32a494ece083d5ad5fdbcddc2dae19dfbd8e2675
Paulo Coutinho <paulo@server.local>
2012-09-11 18:18:28 -0300

    sem arquivos de build

---
+++ build/CMakeFiles/sfml-cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sfml-cmake.dir/Users/paulo/Pessoal/workspaces/qt/sfml-cmake/main.cpp.o"
+  "sfml-cmake.app/Contents/MacOS/sfml-cmake.pdb"
+  "sfml-cmake.app/Contents/MacOS/sfml-cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sfml-cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
