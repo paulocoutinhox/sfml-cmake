96d8f87e316b666533349238213037bf2e769efe
Paulo Coutinho <paulo@prsolucoes.com>
2012-09-13 19:41:48 -0300

    added v8

--- main.cpp
+++ main.cpp
@@ -1,27 +1,88 @@
 #include <SFML/Graphics.hpp>
+#include "sf_v8.h"
+
+v8::Persistent<v8::Context> CreateShellContext() {
+    v8::Handle<v8::ObjectTemplate> global = v8::ObjectTemplate::New();
+    return v8::Context::New(NULL, global);
+}
+
+// Reads a file into a v8 string.
+v8::Handle<v8::String> ReadFile(const char* name) {
+    FILE* file = fopen(name, "rb");
+    if (file == NULL) return v8::Handle<v8::String>();
+
+    fseek(file, 0, SEEK_END);
+    int size = ftell(file);
+    rewind(file);
+
+    char* chars = new char[size + 1];
+    chars[size] = '\0';
+
+    for (int i = 0; i < size;)
+    {
+        int read = static_cast<int>(fread(&chars[i], 1, size - i, file));
+        i += read;
+    }
+
+    fclose(file);
+    v8::Handle<v8::String> result = v8::String::New(chars, size);
+    delete[] chars;
+    return result;
+}
 
 int main()
 {
-    sf::RenderWindow Window(sf::VideoMode(800, 600, 32), "SFML Sample Application");
+    //v8::Persistent<v8::Context> context = CreateShellContext();
+    //context->Global()->Set(v8::String::New("sf"), sf_v8::sf::Init());
 
-    while (Window.isOpen())
+    v8::HandleScope handleScope;
+    v8::Persistent<v8::Context> context = v8::Context::New();
+    v8::Context::Scope context_scope(context);
+
+    context->Global()->Set(v8::String::New("sf"), sf_v8::sf::Init());
+
+    /*
+    // EXEMPLO - IMPRIMIR STRING
+    // Create a string containing the JavaScript code
+    // to execute (notice the quotation).
+    v8::Handle<v8::String> source = v8::String::New(" 'Hello World!'; ");
+
+    // Compile the Javascript code.
+    v8::Handle<v8::Script> script = v8::Script::Compile(source);
+
+    // Run the script to get the result.
+    v8::Handle<v8::Value> result = script->Run();
+
+    v8::String::AsciiValue ascii(result);
+    printf("%s\n", *ascii);
+    */
+
+    sf::RenderWindow window(sf::VideoMode(800, 600, 32), "SFML Sample Application");
+
+    while (window.isOpen())
     {
         sf::Event Event;
-        while (Window.pollEvent(Event))
+        while (window.pollEvent(Event))
         {
             switch (Event.type)
             {
             case sf::Event::Closed:
-                Window.close();
+                window.close();
                 break;
             default:
                 break;
             }
         }
 
-        Window.clear(sf::Color(255, 255, 255));
-        Window.display();
+        v8::Handle<v8::String> source = ReadFile("js/robot1.js");
+        v8::Handle<v8::Script> script = v8::Script::Compile(source);
+        v8::Handle<v8::Value> result = script->Run();
+
+        window.clear(sf::Color(255, 255, 255));
+        window.display();
     }
+
+    context.Dispose();
 
     return 0;
 }
--- sfml-cmake.pro
+++ sfml-cmake.pro
@@ -2,7 +2,9 @@ TEMPLATE = app
 CONFIG += console
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    sf_v8.cpp \
+    v8wrap.cpp
 
 OTHER_FILES += \
     build.sh \
@@ -10,4 +12,8 @@ OTHER_FILES += \
     build/clean.sh \
     CMakeLists.txt \
     .gitignore
+
+HEADERS += \
+    v8wrap.h \
+    sf_v8.h
 
